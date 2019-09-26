c32658d97a6c432629cfb2e1afcf3b4b55c2957b
Luiz Goncalo <s2it_lgoncalo@uolinc.com>
2019-09-26 10:02:27 -0300

    Criação do ProductController

--- products/src/main/java/com/luizGoncalo/products/controller/ProductController.java
+++ products/src/main/java/com/luizGoncalo/products/controller/ProductController.java
@@ -1,16 +1,25 @@
 package com.luizGoncalo.products.controller;
 
+import com.luizGoncalo.products.domain.Product;
+import com.luizGoncalo.products.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/ping")
+@RequestMapping("/product")
 public class ProductController {
 
-    @GetMapping
-    public String ping(){
-        return "pong";
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping(value = "/{id}")
+    public Product productById(@PathVariable(name = "id") final Long id) throws Exception {
+
+        return productService.findById(id);
+
     }
 
 }
