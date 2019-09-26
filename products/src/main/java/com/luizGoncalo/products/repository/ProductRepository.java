b50a0e8c4727c15d3c99af412a48a3a96929b68c
Luiz Goncalo <s2it_lgoncalo@uolinc.com>
2019-09-26 09:52:29 -0300

    Criação do service

--- products/src/main/java/com/luizGoncalo/products/repository/ProductRepository.java
+++ products/src/main/java/com/luizGoncalo/products/repository/ProductRepository.java
@@ -1,4 +1,9 @@
 package com.luizGoncalo.products.repository;
 
-public class ProductRepository {
+import com.luizGoncalo.products.domain.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
 }
--- products/src/main/java/com/luizGoncalo/products/service/ProductService.java
+++ products/src/main/java/com/luizGoncalo/products/service/ProductService.java
@@ -1,5 +1,26 @@
 package com.luizGoncalo.products.service;
 
+import com.luizGoncalo.products.domain.Product;
+import com.luizGoncalo.products.mapper.ProductMapper;
+import com.luizGoncalo.products.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private ProductMapper productMapper;
+
+    public Product findById(Long id) throws Exception{
+
+        final Product response = productRepository.findById(id).orElseThrow(()-> new Exception("Produto não encontrado"));
+
+        return response;
+
+    }
 
 }
